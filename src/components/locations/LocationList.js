cae094bc966146d38304413aa7e75828c0c1b543
ThraxHamilton <mmcclenton16@gmail.com>
2018-08-23 15:52:35 -0500

    checked out. didnt finish

---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>South Nashville</h1>
+                <section>1808 Sweetbriar Ave.</section>
+                <p>Nashville, TN 37212</p>
+                <h1>North Nashville</h1>
+                <section>1500 D.B. Todd Blvd.</section>
+                <p>Nashville, TN</p>
+                <h1>East Nashville</h1>
+                <section>739 Lischey Ave</section>
+                <p>Nashville, TN</p>
+                <h1>West Nashville</h1>
+                <section>400 Indiana Ave</section>
+                <p>Nashville, TN</p>
+            </article>
+
+        
+        );
+    }
+}
